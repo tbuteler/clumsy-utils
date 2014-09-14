ce70283d2e3d82e97b98a250c975bb6fb73cbe14
Tomas Buteler <tbuteler@gmail.com>
2014-09-14 17:58:57 +0100

    New Date construct sets locale

--- src/Clumsy/Utils/Library/Date.php
+++ src/Clumsy/Utils/Library/Date.php
@@ -4,6 +4,13 @@ use Illuminate\Support\Facades\Lang;
 use Carbon\Carbon;
 
 class Date {
+
+	public function __construct()
+	{
+		$locale = class_exists('Clumsy\Locale\Facade') ? \Clumsy\Locale\Facade::codes() : app('config')->get('app.locale');
+	
+		setlocale(LC_TIME, $locale);
+	}
     
     public function format($date, $format)
     {
