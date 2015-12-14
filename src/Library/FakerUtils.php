5e80c51aa395c89019ece28807a7ebf26435bcdd
Tomas Buteler <tomas.buteler@wiz.pt>
2015-12-14 18:03:21 +0000

    Typography faker utils can now separate between typography, table and embedded tags

--- src/Library/FakerUtils.php
+++ src/Library/FakerUtils.php
@@ -7,18 +7,38 @@ use Faker\Provider\Base;
 
 class FakerUtils extends Base
 {
-    protected $typographyHTML;
+    protected $typographyHtml;
+    protected $tableHtml;
+    protected $embeddedHtml;
 
     public function __construct(Generator $generator)
     {
-        $this->typographyHTML = file_get_contents('http://git.io/vRkhK');
+        $this->typographyHtml = file_get_contents('http://git.io/v0Rb0');
+        $this->tableHtml = file_get_contents('http://git.io/v0RNo');
+        $this->embeddedHtml = file_get_contents('http://git.io/v0RNu');
 
         parent::__construct($generator);
     }
 
     public function typography()
     {
-        return $this->typographyHTML;
+        return $this->htmlTags(['typography']);
+    }
+
+    public function htmlTags($groups = null)
+    {
+        $output = '';
+
+        if (is_null($groups)) {
+            $groups = ['typography', 'table', 'embedded'];
+        }
+
+        foreach ((array)$groups as $group) {
+            $property = "{$group}Html";
+            $output .= $this->$property;
+        }
+
+        return $output;
     }
 
     public function richText($maxNbChars = 200, $paragraphs = 4)
